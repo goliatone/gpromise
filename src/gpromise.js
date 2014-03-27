27e222a858bb2376e462abe5c19c21189ad13701
goliatone <burgosemi@gmail.com>
2014-03-26 21:36:49 -0400

    GPromise: Added when method, to process array of promises.

--- src/gpromise.js
+++ src/gpromise.js
@@ -141,7 +141,7 @@
      * @param  {Function} onRejected Callback for reject state
      * @return {GPromise}
      */
-    GPromise.prototype.catch = function(onRejected){
+    GPromise.prototype['catch'] = function(onRejected){
         return this.then(null, onRejected);
     };
 
@@ -184,6 +184,29 @@
         } catch(e){
             then.reject(e);
         }
+    };
+
+    /**
+     * Wait for all these promises to complete. One failed => this fails too.
+     */
+    GPromise.when = function(all) {
+        var promise = new this();
+        var counter = 0;
+        var results = [];
+
+        for (var i=0; i<all.length; i++) {
+            counter++;
+            all[i].then(function(index, result) {
+                results[index] = result;
+                counter--;
+                if (!counter) { promise.fulfill(results); }
+            }.bind(null, i), function(reason) {
+                counter = 1/0;
+                promise.reject(reason);
+            });
+        }
+
+        return promise;
     };
 
     return GPromise;
